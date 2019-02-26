4452126c17991e5f186d8572eab613d280e74d58
Anil Gottipati <38940200+anigot01@users.noreply.github.com>
2019-02-26 16:14:09 +0000

    Update StagingGreenHostFileSauceLabs.bat

--- StagingGreenHostFileSauceLabs.bat
+++ StagingGreenHostFileSauceLabs.bat
@@ -1,5 +1,6 @@
 @echo off
-echo 13.81.101.91 prepd-sitecore.developer.arm.com > %temp%\temphosts.txt
+echo 52.138.198.23  www.arm.com > %temp%\temphosts.txt
+echo 52.138.198.23  developer.arm.com > %temp%\temphosts.txt
 
 type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
 copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
